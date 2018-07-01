80e44821d9484d13fb5a9a0b72d3818b031f8b3b
Demon Singur <demonsingur@gmail.com>
2018-07-01 07:09:46 +0300

    msm8998-common: use wahoo perf stack
    Change-Id: I26b970a8ab55d24042189ac8222b176e3ebc1589

--- msm8998.mk
+++ msm8998.mk
@@ -304,7 +304,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.1-service.xiaomi_msm8998
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
